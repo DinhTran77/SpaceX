a3eaa49d79a979571e110a123481f73b5a21b95e
DinhTran77 <dinhtran77@hotmail.com>
2019-11-11 00:38:20 -0500

    Nav changed again

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
             <NavBarStyle>
                 <header className="header">
                     <nav className="navbar">
-                        <Link to="/">Tran</Link>
+                        <Link to="/">Home</Link>
                         <Link to="/rockets">Rockets</Link>
                     </nav>
                 </header>
